73d9af7716d0a7135f30e776196b256ec953b52a
Marco Rougeth <marco@rougeth.com>
2021-05-29 22:02:51 +0100

    Add build command

--- lightning/bot.py
+++ lightning/bot.py
@@ -10,7 +10,27 @@ from discord.ext.tasks import loop
 from decouple import config
 
 
+DISCORD_TOKEN = config("DISCORD_TOKEN")
+CATEGORY_NAME = "Palestras Relâmpago"
+CHANNEL_NAME = "quero-participar"
+
+
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+
+
+async def get_or_create_lightning_talk_channel(guild: discord.Guild):
+    channels = await guild.fetch_channels()
+    category = discord.utils.get(
+        channels, name=CATEGORY_NAME, type=discord.ChannelType.category
+    )
+    if not category:
+        category = await guild.create_category(CATEGORY_NAME)
+
+    channel = discord.utils.get(category.text_channels, name=CHANNEL_NAME)
+    if not channel:
+        await category.create_text_channel(CHANNEL_NAME)
+
+    return channel
 
 
 @bot.group(invoke_without_command=True)
@@ -18,6 +38,11 @@ async def pr(ctx, *args):
     await ctx.channel.send("TODO: help")
 
 
+@pr.command()
+async def build(ctx, *args):
+    await get_or_create_lightning_talk_channel(ctx.guild)
+    await ctx.message.add_reaction("✅")
+
+
 if __name__ == "__main__":
-    DISCORD_TOKEN = config("DISCORD_TOKEN")
     bot.run(DISCORD_TOKEN)
---
+++ tests/test_discord.py
@@ -0,0 +1,51 @@
+import asyncio
+from unittest import mock
+
+import discord
+import pytest
+
+from lightning.bot import CATEGORY_NAME, CHANNEL_NAME, get_or_create_lightning_talk_channel
+
+
+@pytest.mark.asyncio
+async def test_get_or_create_lightning_talk_channel_no_category():
+    mocked_category = mock.AsyncMock()
+
+    mocked_guild = mock.AsyncMock()
+    mocked_guild.create_category.return_value = mocked_category
+
+    await get_or_create_lightning_talk_channel(mocked_guild)
+
+    mocked_guild.create_category.assert_awaited_once_with(CATEGORY_NAME)
+    mocked_category.create_text_channel.assert_awaited_once_with(CHANNEL_NAME)
+
+
+@pytest.mark.asyncio
+async def test_get_or_create_lightning_talk_channel_no_channel():
+    mocked_category = mock.AsyncMock(text_channels=[], type=discord.ChannelType.category)
+    mocked_category.name = CATEGORY_NAME
+
+    mocked_guild = mock.AsyncMock()
+    mocked_guild.fetch_channels.return_value = [mocked_category]
+
+    await get_or_create_lightning_talk_channel(mocked_guild)
+
+    mocked_guild.create_category.assert_not_awaited()
+    mocked_category.create_text_channel.assert_awaited_once_with(CHANNEL_NAME)
+
+
+@pytest.mark.asyncio
+async def test_get_or_create_lightning_talk_channel_with_category_and_channel():
+    mocked_channel = mock.AsyncMock()
+    mocked_channel.name = CHANNEL_NAME
+
+    mocked_category = mock.AsyncMock(text_channels=[mocked_channel], type=discord.ChannelType.category)
+    mocked_category.name = CATEGORY_NAME
+
+    mocked_guild = mock.AsyncMock()
+    mocked_guild.fetch_channels.return_value = [mocked_category]
+
+    await get_or_create_lightning_talk_channel(mocked_guild)
+
+    mocked_guild.create_category.assert_not_awaited()
+    mocked_category.create_text_channel.assert_not_awaited()
