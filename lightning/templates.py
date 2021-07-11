9a97a8617a1d01c85f76007fc5f246fa22c8abb1
Marco Rougeth <marco@rougeth.com>
2021-07-11 15:01:28 +0100

    Write down proper messages

--- lightning/templates.py
+++ lightning/templates.py
@@ -2,49 +2,40 @@ ANNOUNCEMENT = """
 ⏰ Palestras Relâmpago começando em **{{ waiting_time }} : 00**
 """
 
-
-NEW_LIGHTNING_TALK = """
+BASE_MAIN_MESSAGE = """
 ⚡️⚡️⚡️ **Palestras Relâmpago** ⚡️⚡️⚡️
 
-TODO: Texto sobre palestras relâmpago
-TODO: Instruções de como vai funcionar no discord/streamyard
-TODO: Instruções de como participar
+Palestras Relâmpago é uma seção do evento em que qualquer pessoa pode fazer uma apresentação, sobre qualquer assunto, em **no máximo 5 minutos**.
+
+Lembrando que Código de Conduta também se aplica as palestras relâmpago! Se você tiver alguma dúvida, converse com o time de resposta do @CDC ou com a @Organização.
+
+**Legal! Mas como funciona aqui no Discord?**
+- Após o encerramento das inscrições, vou embaralhar a lista de pessoas que querem participar e mandar aqui nesse canal a ordem de chamada. Todos os inscritos receberão uma notificação quando a lista for atualizada.
+- Alguns minutos antes das palestras relâmpago começarem, os convites serão enviados por mim no privado, aqui no Discord. E caso aconteça alguma desistência, os convites continuarão sendo enviados para as próximas pessoas da lista de chamada.
+
+**Show!! Quer participar, o que eu tenho que fazer?**
+Para se inscrever, basta clicar no emoji ☝️ abaixo que o seu nome aparecerá na lista.
 """
 
 
-NOT_ACTIVE_LIGHTNING_TALK = """
-⚡️⚡️⚡️ **Palestras Relâmpago** ⚡️⚡️⚡️
+NEW_LIGHTNING_TALK = BASE_MAIN_MESSAGE
 
-TODO: Texto sobre palestras relâmpago
-TODO: Instruções de como vai funcionar no discord/streamyard
-TODO: Instruções de como participar
 
-Aguardando lista de chamada!
+NOT_ACTIVE_LIGHTNING_TALK = BASE_MAIN_MESSAGE + """
+🥁 **Aguardando lista de chamada!** 🥁
 """
 
 
-LIGHTNING_TALK_IN_PROGRESS = """
-⚡️⚡️⚡️ **Palestras Relâmpago** ⚡️⚡️⚡️
-
-TODO: Texto sobre palestras relâmpago
-TODO: Instruções de como vai funcionar no discord/streamyard
-TODO: Instruções de como participar
-
-Inscrições:
+LIGHTNING_TALK_IN_PROGRESS = BASE_MAIN_MESSAGE + """
+**Inscrições**:
 {%- for speaker in speakers %}
 - <@{{ speaker }}>
 {%- endfor %}
 """
 
 
-LIGHTNING_TALK_SPEAKERS_ORDER = """
-⚡️⚡️⚡️ **Palestras Relâmpago** ⚡️⚡️⚡️
-
-TODO: Texto sobre palestras relâmpago
-TODO: Instruções de como vai funcionar no discord/streamyard
-TODO: Instruções de como participar
-
-Inscrições:
+LIGHTNING_TALK_SPEAKERS_ORDER = BASE_MAIN_MESSAGE + """
+**Ordem de chamada**:
 {%- for speaker, data in speakers.items() %}
 {%- if data.invited and not data.confirmed %}
 ⏰ <@{{ speaker }}>
